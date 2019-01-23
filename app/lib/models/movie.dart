f5b6a39c9ebbaefcb2d136d5603b90528b572db8
Mateusz Szklarek <mateusz.szklarek@gmail.com>
2019-01-23 01:44:02 +0100

    Implement API models

---
+++ app/lib/models/movie.dart
@@ -0,0 +1,19 @@
+class Movie {
+  final int id;
+  final String name;
+  final String description;
+  final String cover;
+  final double rating;
+  final String genre;
+
+  Movie(this.id, this.name, this.description, this.cover, this.rating,
+      this.genre);
+
+  Movie.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        description = json['short_description'],
+        cover = json['cover'],
+        rating = json['rating'],
+        genre = json['genre'];
+}
---
+++ app/lib/models/movies_response.dart
@@ -0,0 +1,18 @@
+import 'package:app/models/movie.dart';
+
+class MoviesResponse {
+  final int count;
+  final String nextPage;
+  final String previousPage;
+  final List<Movie> movies;
+
+  MoviesResponse(this.count, this.nextPage, this.previousPage, this.movies);
+
+  MoviesResponse.fromJson(Map<String, dynamic> json)
+      : count = json['count'],
+        nextPage = json['next'],
+        previousPage = json['previous'],
+        movies = (json['results'] as List)
+            .map<Movie>((movie) => Movie.fromJson(movie))
+            .toList();
+}
