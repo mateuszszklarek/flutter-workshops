57aa619350681905bc6156c4ab5ba227b0e32f69
Mateusz Szklarek <mateusz.szklarek@gmail.com>
2019-01-23 14:52:01 +0100

    Implement list view with data source

--- app/lib/pages/movies_page.dart
+++ app/lib/pages/movies_page.dart
@@ -1,11 +1,66 @@
+import 'package:app/api/movies_client.dart';
+import 'package:app/models/movie.dart';
 import 'package:flutter/material.dart';
 
-class MoviesPage extends StatelessWidget {
+class MoviesPage extends StatefulWidget {
+  @override
+  State createState() => _MoviePageState();
+}
+
+class _MoviePageState extends State<MoviesPage> {
+  int _currentPage = 1;
+  List<Movie> _movies = [];
+  MoviesClient _client = MoviesClient();
+  ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _fetch();
+    _setUpScrollListener();
+  }
+
+  void _setUpScrollListener() {
+    _scrollController.addListener(() {
+      if (_scrolledToEnd()) {
+        _fetch();
+      }
+    });
+  }
+
+  bool _scrolledToEnd() =>
+      _scrollController.position.pixels ==
+      _scrollController.position.maxScrollExtent;
+
+  void _fetch() {
+    _client.fetch(page: _currentPage).then((response) {
+      setState(() {
+        _movies.addAll(response.movies);
+        _currentPage++;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Movies Page"),
+        child: ListView.builder(
+          controller: _scrollController,
+          itemCount: _movies.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Container(
+                  child:
+                      Text("Item: ${index + 1} movie: ${_movies[index].name}"),
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
