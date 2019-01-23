cbd05f687323ce99f29f324fe3756cc995096a69
Mateusz Szklarek <mateusz.szklarek@gmail.com>
2019-01-23 03:44:33 +0100

    Refactor MoviesClient

--- app/lib/api/movies_client.dart
+++ app/lib/api/movies_client.dart
@@ -3,11 +3,13 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class MoviesClient {
+  final String _baseUrl = 'https://still-badlands-71873.herokuapp.com';
+
   Future<MoviesResponse> fetch({int page}) async {
-    final response = await http.get(
-        'https://still-badlands-71873.herokuapp.com/api/v1/movies/?page=$page');
+    final response = await http.get(_baseUrl + "/api/v1/movies/?page=$page");
 
     if (response.statusCode == 200) {
+      print(response.body);
       return MoviesResponse.fromJson(json.decode(response.body));
     } else {
       throw Exception('Failed to load movies');
